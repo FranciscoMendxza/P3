b7c8c707acdc71a2c26c83a4d44e65fd3c71959d
Francisco <mendxza12@gmail.com>
2023-09-23 13:19:28 -0600

    Practica 3

---
+++ app/src/main/java/mx/edu/isc/tesoem/mojf/pp3/Resta2numActivity.java
@@ -0,0 +1,32 @@
+package mx.edu.isc.tesoem.mojf.pp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Resta2numActivity extends AppCompatActivity {
+
+    EditText resta1, resta2;
+    TextView lblresta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resta2num);
+
+        resta1 = findViewById(R.id.txtnum1p4);
+        resta2 = findViewById(R.id.txtnum2p4);
+        lblresta = findViewById(R.id.lblrestap3);
+    }
+
+    public void calcularr (View v){
+
+        int num3 = Integer.valueOf(resta1.getText().toString());
+        int num4 = Integer.valueOf(resta2.getText().toString());
+
+        lblresta.setText(String.valueOf(num3 - num4));
+    }
+}
